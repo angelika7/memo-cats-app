ee5d0621d64d3591cfde905876909f2a4c4da2bb
Angelika <angelika.chochorowska7@gmail.com>
2020-08-29 18:03:42 +0200

    Change images path

--- src/components/Memo/Cards/Cards.js
+++ src/components/Memo/Cards/Cards.js
@@ -1,19 +1,19 @@
-import img1 from './../../../assets/images/17-min.jpg';
-import img2 from './../../../assets/images/2-min.jpg';
-import img3 from './../../../assets/images/3-min.jpg';
-import img4 from './../../../assets/images/4-min.jpg';
-import img5 from './../../../assets/images/5-min.jpg';
-import img6 from './../../../assets/images/6-min.jpg';
-import img7 from './../../../assets/images/7-min.jpg';
-import img8 from './../../../assets/images/8-min.jpg';
-import img9 from './../../../assets/images/9-min.jpg';
-import img10 from './../../../assets/images/10-min.jpg';
-import img11 from './../../../assets/images/11-min.jpg';
-import img12 from './../../../assets/images/12-min.jpg';
-import img13 from './../../../assets/images/13-min.jpg';
-import img14 from './../../../assets/images/14-min.jpg';
-import img15 from './../../../assets/images/15-min.jpg';
-import img16 from './../../../assets/images/16-min.jpg'; 
+import img1 from './../../../assets/images/17.jpg';
+import img2 from './../../../assets/images/2.jpg';
+import img3 from './../../../assets/images/3.jpg';
+import img4 from './../../../assets/images/4.jpg';
+import img5 from './../../../assets/images/5.jpg';
+import img6 from './../../../assets/images/6.jpg';
+import img7 from './../../../assets/images/7.jpg';
+import img8 from './../../../assets/images/8.jpg';
+import img9 from './../../../assets/images/9.jpg';
+import img10 from './../../../assets/images/10.jpg';
+import img11 from './../../../assets/images/11.jpg';
+import img12 from './../../../assets/images/12.jpg';
+import img13 from './../../../assets/images/13.jpg';
+import img14 from './../../../assets/images/14.jpg';
+import img15 from './../../../assets/images/15.jpg';
+import img16 from './../../../assets/images/16.jpg'; 
 
 class Card {
     constructor (flipped, type, src) {
